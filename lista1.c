5fa04e0d91921690ea6e1fe02cf542e83d56dc0d
gabrielly2132 <159589324+gabrielly2132@users.noreply.github.com>
2024-02-17 00:28:42 +0000

    ate q13

--- lista1.c
+++ lista1.c
@@ -33,36 +33,90 @@ void q04 () {
 void q05 () {
 float num1;
 float num2;
-printf ("digite 2 numeros reais: ");
-scanf ()
+printf ("digite dois numeros reais:");
+scanf ("%f %f", &num1, &num2);
+printf ("os múmeros digitados foram: %f, %f", num1, num2);
 }
 //6. Faça um programa que leia um número inteiro e imprima o seu
 //   antecessor e o seu sucessor.
-
+void q06 () {
+   int num, ant, suc;
+   printf ("digite um numero:");
+   scanf ("%d", &num);
+   ant=num-1;
+   suc=num+1;
+   printf ("o numero que você digitou foi: %d\n. O antecessor é %d\n. O sucessor é %d\n", num, ant, suc); 
+}
 //7. Faça um programa que leia o nome o endereço e o telefone de
 //   um cliente e ao final, imprima esses dados.
+void q07 () {
+    char nome[50], endereco[70], telefone[14];
+    
+    printf ("nome: ");
+    scanf (" %50[^\n]", &nome);
+    printf ("endereço: ");
+    scanf (" %70[^\n]", &endereco);
+    printf ("telefone: ");
+    scanf (" %14[^\n]", &telefone);
+    printf ("nome: %s\nEndereco: %s\ntelefone: %s\n", nome, endereco, telefone);
+}
 
 //8. Faça um programa que leia dois números inteiros e imprima a
 //   subtração deles.
-
+void q08 () {
+    int num1, num2, sub;
+    printf ("digite dois numeros inteiros:");
+    scanf ("%d %d", &num1, &num2);
+    sub= num1-num2;
+    printf ("o numero que você digitou foi: %d %d\n. A subtração deles é %d\n", num1, num2, sub);
+}
 //9. Faça um programa que leia umnúmero real e imprima ¼ deste número.
-
+void q09 () {
+    float num, num1;
+    printf ("digite um numeros real:");
+    scanf ("%f", &num);
+    num1= num-4.0;
+    printf ("os múmero digitado foi: %f. O ¼ deste número é: %f\n", num, num1);
+}
 //10. Faça um programa que leia três números reais e calcule a
 //    média aritmética destes números. Ao final, o programa deve
 //    imprimir o resultado do cálculo.
-
+void q10 () {
+    int num1, num2, num3, mediarit;
+    printf ("digite três numeros inteiros:");
+    scanf ("%d %d %d", &num1, &num2, &num3);
+    mediarit= num1+num2+num3/3.0;
+     printf ("o numero que você digitou foi: %d %d %d\n. A média aritmética destes números é %d\n", num1, num2, num3, mediarit);
+}
 //11. Faça um programa que leia dois números reais e calcule as
 //    quatro operações básicas entre estes dois números, adição,
 //    subtração,multiplicação e divisão. Ao final, o programa
 //    deve imprimir os resultados dos cálculos.
-
+void q11 (){
+    float num1, num2, soma, sub, mult, div;
+    printf ("digite dois numeros reais:");
+    scanf ("%f %f", &num1, &num2);
+    soma= num1+num2;
+    sub= num1-num2; 
+    mult= num1*num2;
+    div= num1/num2;
+    printf ("os múmeros digitados foram: %f %f\n. A soma é %f\n. A subtração é %f\n. A multiplicação é %f\n. A divisão é %f\n", num1, num2, soma, sub, mult, div);
+}
 //12. Faça um programa que leia um número real e calcule o
 //    quadrado deste número. Ao final, o programa deve
 //    imprimir o resultado do cálculo.
-
+void q12 () {
+    float num1, quadrado;
+    printf ("digite um numeros real:");
+    scanf ("%f", &num1); 
+    quadrado= num1*num1;
+     printf ("o múmero digitado foi:%f\n.O quadrado é %f\n", num1, quadrado);
+}
 //13. Faça um programa que leia o saldo de uma conta poupança e
 //    imprima o novo saldo, considerando um reajuste de 2%.
-
+ void q13 () {
+      float num1, quadrado;
+ }
 //14. Faça um programa que leia a base e a altura de um retângulo
 //    e imprima o perímetro (base + altura) e a área (base * altura).
 
@@ -106,7 +160,7 @@ scanf ()
 //    necessário também ler o valor da cotação do dólar.
 
 int main () {
-    q04();
+    q12();
 
     return EXIT_SUCCESS;
 }
